1f60c358b8dba9f5fb479d8d7096a2e6375630f4
蒋东 <852778359@qq.com>
2023-03-15 10:22:26 +0800

    build: utils

--- packages/utils/rollup.config.js
+++ packages/utils/rollup.config.js
@@ -1,16 +1,20 @@
+import { nodeResolve } from '@rollup/plugin-node-resolve';
+import babel from '@rollup/plugin-babel';
+import typescript from '@rollup/plugin-typescript';
 import rollupConfig from '../../rollup.config.js';
 
 export default  {
 	...rollupConfig,
 	input: './src/index.ts',
-	output: [
-		{
-			file: 'dist/index-es.js',
-			format: 'es',
-		},
-		{
-			file: 'dist/index-cjs.js',
-			format: 'cjs',
-		},
-	],
+	output: {
+		file: 'dist/index.js',
+		format: 'cjs',
+	},
+	plugins: [
+		nodeResolve({
+			extensions:['.js', '.ts']
+		}),
+		typescript(),
+		babel({ babelHelpers: 'bundled' })
+	]
 };
