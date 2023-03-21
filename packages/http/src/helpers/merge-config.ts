19319dd3367e14b4f619d55a934be9d06bd4f1ce
蒋东 <852778359@qq.com>
2023-03-21 17:06:15 +0800

    style: 变量名修改

--- packages/http/src/helpers/merge-config.ts
+++ packages/http/src/helpers/merge-config.ts
@@ -1,26 +1,26 @@
 import { isObject } from './utils';
 
 export const mergeConfig = <T>(source: object, merged: object): T => {
-	let defaultKeys = Object.keys(merged);
+	let mergedKeys = Object.keys(merged);
 	const globalKeys = Object.keys(source);
 	let config = {};
 	for (let i = 0; i < globalKeys.length; i++) {
 		const key = globalKeys[i];
 		const value = source[key];
-		const defaultValue = merged[key];
+		const mergedValue = merged[key];
 		if (value) {
-			if (isObject(value) && defaultValue && isObject(defaultValue)) {
-				config[key] = [...value, ...defaultValue];
+			if (isObject(value) && mergedValue && isObject(mergedValue)) {
+				config[key] = [...value, ...mergedValue];
 			} else {
 				config[key] = value;
 			}
 		}
 		// 过滤掉已经匹配上的默认值
-		defaultKeys = defaultKeys.filter((it) => it !== key);
+		mergedKeys = mergedKeys.filter((it) => it !== key);
 	}
-	if (defaultKeys.length) {
-		for (let i = 0; i < defaultKeys.length; i++) {
-			const key = defaultKeys[i];
+	if (mergedKeys.length) {
+		for (let i = 0; i < mergedKeys.length; i++) {
+			const key = mergedKeys[i];
 			config[key] = merged[key];
 		}
 	}
